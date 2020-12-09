eda468c00b3c9c29564729f998b595b84e9e40c5
PureTilt <kirostat@gmail.com>
2020-12-09 16:40:07 +0500

    0.1 Commit of Initial Updated Mod Files

---
+++ Freeport Republic/data/hullmods/AkoyaEnginesDegraded.java
@@ -0,0 +1,25 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.impl.hullmods.BaseLogisticsHullMod;
+
+public class AkoyaEnginesDegraded extends BaseLogisticsHullMod {
+
+	public static final float PROFILE_MULT = 0.8f;
+	public static final float HEALTH_BONUS = 70f;
+	public static final float HULL_BONUS = 5f;
+	
+	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		stats.getEngineHealthBonus().modifyPercent(id, HEALTH_BONUS);
+		stats.getSensorProfile().modifyMult(id, PROFILE_MULT);
+	}
+	
+	public String getDescriptionParam(int index, HullSize hullSize) {
+		if (index == 0) return "" + (int) HEALTH_BONUS + "%";
+		if (index == 1) return "" + (int) ((1f - PROFILE_MULT) * 100f) + "%";
+		return null;
+	}
+
+
+}
---
+++ Freeport Republic/jars/src/data/scripts/weapons/fpr_MS_RifleEffect.java
@@ -0,0 +1,101 @@
+package data.scripts.weapons;
+
+import com.fs.starfarer.api.AnimationAPI;
+import com.fs.starfarer.api.combat.*;
+import org.lazywizard.lazylib.MathUtils;
+
+/**
+ * @author Tartiflette  (old position data: TORSO_OFFSET=-45, LEFT_ARM_OFFSET=-75, RIGHT_ARM_OFFSET=-25, MAX_OVERLAP = 10)
+ */
+public class fpr_MS_RifleEffect implements EveryFrameWeaponEffectPlugin {
+
+    private final float TORSO_OFFSET=-0, LEFT_ARM_OFFSET=-76, RIGHT_ARM_OFFSET=-5, MAX_OVERLAP = 10;
+    private boolean runOnce = false, lockNloaded = false;
+    private ShipSystemAPI system;
+    private ShipAPI ship;
+    private AnimationAPI anim;
+    private WeaponAPI armL, armR, pauldronL, pauldronR, torso;
+    private float delay = 0.1f;
+    private float timer = 0;
+    private float SPINUP = 0.02f;
+    private float SPINDOWN = 10f;
+    private float overlap = 0, heat = 0;
+
+    @Override
+    public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
+
+        if (!runOnce) {
+            runOnce = true;
+            ship = weapon.getShip();
+            system = ship.getSystem();
+            for (WeaponAPI w : ship.getAllWeapons()) {
+                switch (w.getSlot().getId()) {
+                    case "TORSO":
+                        torso = w;
+                        break;
+                    case "L_S_ARM":
+                        armL = w;
+                        break;
+                    case "R_S_ARM":
+                        armR = w;
+                        break;
+                    case "L_SHOULDER":
+                        pauldronL = w;
+                        break;
+                    case "R_SHOULDER":
+                        pauldronR = w;
+                        break;
+                }
+            }
+        }
+
+        if (engine.isPaused()) {
+            return;
+        }
+
+        if (ship.getEngineController().isAccelerating()) {
+            if (overlap > (MAX_OVERLAP - 0.1f)) {
+                overlap = MAX_OVERLAP;
+            } else {
+                overlap = Math.min(MAX_OVERLAP, overlap + ((MAX_OVERLAP - overlap) * amount * 5));
+            }
+        } else if (ship.getEngineController().isDecelerating() || ship.getEngineController().isAcceleratingBackwards()) {
+            if (overlap < -(MAX_OVERLAP - 0.1f)) {
+                overlap = -MAX_OVERLAP;
+            } else {
+                overlap = Math.max(-MAX_OVERLAP, overlap + ((-MAX_OVERLAP + overlap) * amount * 5));
+            }
+        } else {
+            if (Math.abs(overlap) < 0.1f) {
+                overlap = 0;
+            } else {
+                overlap -= (overlap / 2) * amount * 3;
+            }
+        }
+
+        float sineA = 0, sinceB = 0;
+        sineA = 1;
+        sinceB = 1;
+
+
+        float global = ship.getFacing();
+        float aim = MathUtils.getShortestRotation(global, weapon.getCurrAngle());
+
+        torso.setCurrAngle(global + sineA * TORSO_OFFSET + aim * 0.3f);
+
+        armR.setCurrAngle(weapon.getCurrAngle() + RIGHT_ARM_OFFSET);
+
+        pauldronR.setCurrAngle(global + sineA * TORSO_OFFSET * 0.5f + aim * 0.75f + RIGHT_ARM_OFFSET * 0.5f);
+
+        armL.setCurrAngle(
+                global
+                        +
+                        ((aim + LEFT_ARM_OFFSET) * sinceB)
+                        +
+                        ((overlap + aim * 0.25f) * (1 - sinceB))
+        );
+
+        pauldronL.setCurrAngle(torso.getCurrAngle() + MathUtils.getShortestRotation(torso.getCurrAngle(), armL.getCurrAngle()) * 0.6f);
+
+    }
+}
